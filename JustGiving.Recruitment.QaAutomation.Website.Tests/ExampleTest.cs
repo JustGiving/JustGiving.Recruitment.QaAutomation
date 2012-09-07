8f3830d2aace7a281fe680932ae2ca0653f25928
David Whitney <david@davidwhitney.co.uk>
2012-09-07 17:59:36 +0100

    Added empty web project for test.

---
+++ JustGiving.Recruitment.QaAutomation.Website.Tests/ExampleTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace JustGiving.Recruitment.QaAutomation.Website.Tests
+{
+    [TestFixture]
+    public class ExampleTest
+    {
+        private IWebDriver _driver;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 0, 30));
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            if (_driver != null)
+            {
+                _driver.Close();
+            }
+        }
+
+        [Test]
+        public void WhenVisitingTheDemoWebsite_HomePageTitleIsCorrect()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:9876");
+
+            Assert.That(_driver.Title, Is.EqualTo("Home Page"));
+        }
+    }
+}
---
+++ JustGiving.Recruitment.QaAutomation.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JustGiving.Recruitment.QaAutomation.Website.MvcApplication" Language="C#" %>
