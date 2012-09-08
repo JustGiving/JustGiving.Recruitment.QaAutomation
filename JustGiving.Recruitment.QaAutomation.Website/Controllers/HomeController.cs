a65c2a462610c7afeaf35c82a7491e393a84d887
David Whitney <david@davidwhitney.co.uk>
2012-09-08 12:21:13 +0100

    Added randomly displayed list.

--- JustGiving.Recruitment.QaAutomation.Website/Controllers/HomeController.cs
+++ JustGiving.Recruitment.QaAutomation.Website/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JustGiving.Recruitment.QaAutomation.Website.Models;
 
 namespace JustGiving.Recruitment.QaAutomation.Website.Controllers
 {
@@ -15,9 +16,18 @@ namespace JustGiving.Recruitment.QaAutomation.Website.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult About()
         {
-            return View();
+            
+            
+            return View(new AboutViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult About(AboutViewModel model)
+        {
+            return View(model);
         }
     }
 }
